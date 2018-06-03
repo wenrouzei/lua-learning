dabbb2baaa8895b61b28253e22074572c976ca1f
wenrouzei <wenrouzei@163.com>
2018-06-03 15:19:45 +0800

    add 添加hello-world.lua

---
+++ hello-world.lua
@@ -0,0 +1,18 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: wenrouzei
+-- Date: 2018/6/3
+-- Time: 14:56
+-- To change this template use File | Settings | File Templates.
+--
+--#!/usr/local/bin/lua
+print('Hello World!')
+print("Hello World!")
+
+--print('单行注释')
+
+--[[
+print('多行注释')
+print('多行注释')
+print('多行注释')
+--]]
